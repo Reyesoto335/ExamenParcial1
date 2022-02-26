ed15bd1d32b8fd1c2481c6b254b5f885500fae36
Reyesoto335 <elmersoto123@gmail.com>
2022-02-26 10:29:56 -0600

    Primer commmit y subida del codgio

---
+++ Main.java
@@ -0,0 +1,12 @@
+package ParcialI;
+
+public class Ejecutor {
+
+    public static void main(String[] args) {
+        Ordenes ordenes = new Ordenes();
+        
+        ordenes.pedir_numero();
+        ordenes.menu(0);
+    }
+    
+}
